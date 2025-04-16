37274f8a7a315f1715fde5ce272224b52e6f2a05
inredzero <96677474+inredzero@users.noreply.github.com>
2025-04-16 22:23:19 +0900

    [BOJ] 12605_단어순서뒤집기_안홍영

--- src/week02/BOJ_B2_12605_단어순서뒤집기/Hong.java
+++ src/week02/BOJ_B2_12605_단어순서뒤집기/Hong.java
@@ -1,5 +1,32 @@
 package week02.BOJ_B2_12605_단어순서뒤집기;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 public class Hong {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            Stack<String> stack = new Stack<>();
+
+            while (st.hasMoreTokens()) {
+                stack.push(st.nextToken());
+            }
+
+            System.out.print("Case #" + i + ":");
+            while (!stack.isEmpty()) {
+                System.out.print(" " + stack.pop());
+            }
+            System.out.println();
+        }
+
+
+
+    }
 }
