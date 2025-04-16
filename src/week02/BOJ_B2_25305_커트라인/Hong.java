841b336e43c222579263c4f6eee151e8e308b762
inredzero <96677474+inredzero@users.noreply.github.com>
2025-04-16 22:54:40 +0900

    [BOJ] 25305_커트라인_안홍영

--- src/week02/BOJ_B2_25305_커트라인/Hong.java
+++ src/week02/BOJ_B2_25305_커트라인/Hong.java
@@ -1,5 +1,32 @@
 package week02.BOJ_B2_25305_커트라인;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
 
 public class Hong {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int N = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        String x = br.readLine();
+
+        String [] arr = x.split(" ");
+
+        Integer[] nums = new Integer[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            nums[i] = Integer.parseInt(arr[i]);
+        }
+
+        Arrays.sort(nums, Comparator.reverseOrder());
+
+        System.out.println(nums[k-1]);
+
+    }
 }
