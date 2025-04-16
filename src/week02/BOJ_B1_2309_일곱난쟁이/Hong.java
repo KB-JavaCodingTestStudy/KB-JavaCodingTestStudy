884f39d459a5f095174ddd4782339ed7695dd6c3
inredzero <96677474+inredzero@users.noreply.github.com>
2025-04-16 23:42:19 +0900

    [BOJ] 2309_일곱난쟁이_안홍영

--- src/week02/BOJ_B1_2309_일곱난쟁이/Hong.java
+++ src/week02/BOJ_B1_2309_일곱난쟁이/Hong.java
@@ -1,5 +1,44 @@
 package week02.BOJ_B1_2309_일곱난쟁이;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Hong {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        List<Integer> list = new ArrayList<>();
+        int sum = 0;
 
+        for (int i = 0; i < 9; i++) {
+            int n = Integer.parseInt(br.readLine());
+            list.add(n);
+            sum += n;
+        }
+
+        int fake = sum - 100;
+        int a = 0, b = 0;
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = i + 1; j < list.size(); j++) {
+                if (list.get(i) + list.get(j) == fake) {
+                    a = list.get(i);
+                    b = list.get(j);
+                    break;
+                }
+            }
+        }
+
+        list.remove(Integer.valueOf(a));
+        list.remove(Integer.valueOf(b));
+
+        Collections.sort(list);
+
+        for (int h : list) {
+            System.out.println(h);
+        }
+
+    }
 }
