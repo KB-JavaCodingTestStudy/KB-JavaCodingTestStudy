ef8f942b35968ea64547592a8d8d1aa4e2dd13cf
inredzero <96677474+inredzero@users.noreply.github.com>
2025-04-16 21:47:47 +0900

    [BOJ] 17608_막대기_안홍영

--- src/week02/BOJ_B2_17608_막대기/Hong.java
+++ src/week02/BOJ_B2_17608_막대기/Hong.java
@@ -1,5 +1,31 @@
 package week02.BOJ_B2_17608_막대기;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Hong {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
 
+        int N = Integer.parseInt(br.readLine());
+        int max = Integer.MIN_VALUE;
+        int cnt = 0;
+
+        for (int i = 0; i < N; i++) {
+            stack.push(Integer.parseInt(br.readLine()));
+        }
+
+        for (int i = 0; i < N; i++) {
+            int cStack = stack.pop();
+            if (cStack > max) {
+                cnt++;
+                max = cStack;
+            }
+        }
+
+        System.out.println(cnt);
+
+    }
 }
