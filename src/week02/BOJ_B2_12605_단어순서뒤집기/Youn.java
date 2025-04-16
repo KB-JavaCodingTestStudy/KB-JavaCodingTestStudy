dbe82c48c2e8be55583ad306127a86ece2cd8888
0woy <87132052+0woy@users.noreply.github.com>
2025-04-16 17:05:22 +0900

    [BOJ]_12605_Youn

---
+++ src/week02/BOJ_B2_12605_단어순서뒤집기/Youn.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        int tc =Integer.parseInt(br.readLine());
+        int idx =0;
+        for(int i=1;i<=tc;i++){
+            String[] tokens = br.readLine().split(" ");
+             sb.append("Case #").append(i).append(": ");
+             for(int j = tokens.length-1;j>=0;j--){
+                 sb.append(tokens[j]).append(" ");
+             }
+             sb.append("\n");
+        }
+        System.out.println(sb);
+    }
+}
