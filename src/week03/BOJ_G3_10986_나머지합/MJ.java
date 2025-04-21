ed6c2977a20482345609db32bc8fb9c2b4adbc5f
MJ <nampink2300@gmail.com>
2025-04-21 16:02:37 +0900

    [BOJ] 10986_나머지 합_남민주

---
+++ src/week03/BOJ_G3_10986_나머지합/MJ.java
@@ -0,0 +1,38 @@
+package week03.BOJ_G3_10986_나머지합;
+
+import java.io.*;
+import java.util.*;
+
+public class MJ {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+
+        String numString = br.readLine();
+        StringTokenizer st = new StringTokenizer(numString);
+        
+        int N=Integer.parseInt(st.nextToken());
+        int M=Integer.parseInt(st.nextToken());
+
+        String AString = br.readLine();
+        st = new StringTokenizer(AString);
+
+        int[] sums = new int[M];
+
+        int sum=0;
+        while(st.hasMoreTokens()){
+            sum+= Integer.parseInt(st.nextToken());
+            sums[sum % M]++;
+        }
+
+        long result=sums[0];
+        for(int i=0;i<M;i++){
+            int tmp=sums[i];
+            result+=tmp*(tmp-1)/2;
+        }
+
+        bw.write(String.valueOf(result));
+        bw.flush();
+    }
+}
