d9538a2a5d8fad73598443b05dc9039ce9855d53
0woy <87132052+0woy@users.noreply.github.com>
2025-04-24 09:01:00 +0900

    [BOJ] 10986_나머지합_박윤아
    문제 풀이는 https://0woy.tistory.com/entry/10986-%EB%82%98%EB%A8%B8%EC%A7%80-%ED%95%A9에 작성

---
+++ src/week03/BOJ_G3_10986_나머지합/Youna.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main{
+    public static <Set> void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        int [] remain = new int[m];
+        st = new StringTokenizer(br.readLine());
+        long prev =0;
+        for(int i = 0; i < n; i++) {
+            int cur =Integer.parseInt(st.nextToken());
+            remain[(int) ((prev + cur)%m)]++;
+            prev +=cur;
+        }
+        long count = remain[0];
+        for(int i = 0; i < m; i++) {
+            count+= (long) remain[i] *(remain[i]-1) / 2;
+        }
+        bw.write(count+"");
+        bw.flush();
+        bw.close();
+    }
+}
