44f547d7a180ca102228c44f01f7e7a0cb198518
권채린 <92051742+chaelink@users.noreply.github.com>
2025-05-10 15:44:28 +0900

    [PGS] LV2_구명보트_권채린

--- src/week05/PGS_LV2_구명보트/Kwon.java
+++ src/week05/PGS_LV2_구명보트/Kwon.java
@@ -1,1 +1,49 @@
+import java.util.*;
 
+class Solution1 {
+    public int solution1(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people); //오름차순 정렬임에 유의
+
+        //직접 people 배열의 값을 삭제하지 않고 인덱스 이동으로 체크
+        int start = 0;
+        int end = people.length-1;
+        
+        while(start<=end) {
+            if(people[start] + people[end] <= limit) { //몸무게가 가장 큰 사람이 보트를 탈 때 몸무게가 가장 작은 사람도 함께 태울 수 있다면
+                start++;                               //앞쪽 인덱스 하나 증가
+            }
+            answer++;     //보트 수 더하기
+            end--;        //맨 뒤의 사람 보트에 태웠으니 인덱스 하나 앞으로
+        }
+        return answer;
+    }
+}
+
+//================= 처음 시도 ====================
+class Solution {
+    public int solution(int[] people, int limit) {
+        int answer = 0;
+        Arrays.sort(people); //오름차순 정렬임
+        int start = 0;
+        int end = people.length-1
+        
+        while(start<=end) {
+            if(start==end) {
+                answer++;
+                start++;
+                end--;
+            } else {
+                if(people[start] + people[end] <= limit) {
+                    answer++;
+                    start++;
+                    end--;
+                } else {
+                    answer++;
+                    end--;
+                }
+            }
+        }
+        return answer;
+    }
+}
