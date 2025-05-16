14ea02b3de1e74c0fa3338c8d4c706b595d7593c
winA <wina418@kw.ac.kr>
2025-05-16 09:25:10 +0900

    다익스트라 문제 추가

---
+++ src/week06/BOJ_G5_1916_최소비용구하기/Wina.java
@@ -0,0 +1,46 @@
+package week06.BOJ_G5_1916_최소비용구하기;
+
+/* ================================================================
+ *
+ * Problem  :
+ * Author   : 최승아
+ * Date     : 202--
+ *
+ * ================================================================
+ * 📌 문제 분석 요약
+ *
+ * # 입력
+ *
+ * # 출력
+ *
+ * 💻 알고리즘 설계
+ *
+ *
+ * ⏰ 시간복잡도
+ *
+ * ================================================================
+ */
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Wina {
+	
+	// 초기 설정
+	public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	public static StringTokenizer st;
+	
+	public static void main(String[] args) throws IOException {
+		
+		//	종료 설정
+		br.close();
+		bw.close();
+		bw.flush();
+	}
+	
+}
