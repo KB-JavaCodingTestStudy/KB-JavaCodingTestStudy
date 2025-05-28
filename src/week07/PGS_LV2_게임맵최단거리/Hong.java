60898406b43fe19c49895e8d5497c11c6341bd12
inredzero <96677474+inredzero@users.noreply.github.com>
2025-05-28 22:12:52 +0900

    [PGS] LV2_게임맵최단거리_안홍영

--- src/week07/PGS_LV2_게임맵최단거리/Hong.java
+++ src/week07/PGS_LV2_게임맵최단거리/Hong.java
@@ -1,1 +1,39 @@
+import java.util.*;
 
+class Solution {
+    public int solution(int[][] maps) {
+        int n = maps.length;
+        int m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> queue = new ArrayDeque<>();
+        
+        
+        queue.add(new int[]{0, 0, 1});
+        visited[0][0] = true;
+        
+        int[] dx = {1, 0, -1, 0};
+        int[] dy = {0, 1, 0, -1};
+        
+        
+        while(!queue.isEmpty()) {
+            int[] cur = queue.remove();
+            int x = cur[0], y = cur[1], dist = cur[2];
+            
+            
+            if(x == n-1 && y == m-1){
+                return dist;
+            }
+            
+            for(int i=0; i<4; i++){
+                int nx = x+dx[i], ny = y+dy[i];
+                
+                if(nx >= 0 && nx < n && ny >= 0 && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]){
+                    visited[nx][ny] = true;
+                    queue.add(new int[]{nx, ny, dist+1});
+                }  
+            }
+        }
+        return -1;
+        
+    }
+}
