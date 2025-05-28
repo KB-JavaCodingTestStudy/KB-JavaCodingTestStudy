c028cf12906dfe412178ce73749d69727cd755df
inredzero <96677474+inredzero@users.noreply.github.com>
2025-05-28 22:14:55 +0900

    [PGS] LV_2_전력망을둘로나누기_안홍영

--- src/week07/PGS_LV2_전력망을둘로나누기/Hong.java
+++ src/week07/PGS_LV2_전력망을둘로나누기/Hong.java
@@ -1,1 +1,51 @@
 
+import java.util.*;
+
+class Solution {
+    public int solution(int n, int[][] wires) {
+        
+        int answer = Integer.MAX_VALUE;
+        
+        for(int i = 0; i<wires.length; i++){
+            List<List<Integer>> graph = new ArrayList<>();
+            for(int j = 0; j<=n; j++){
+                graph.add(new ArrayList<>());
+            }
+            
+            for(int j = 0; j<wires.length; j++){
+                if(i == j) continue;
+                int a = wires[j][0];
+                int b = wires[j][1];
+                graph.get(a).add(b);
+                graph.get(b).add(a);
+            }
+            int count = bfs(graph, n, wires[i][0]);
+            int diff = Math.abs(n - count - count);
+            answer = Math.min(answer, diff);
+            
+        }
+        return answer;
+    }
+    
+    private int bfs(List<List<Integer>> graph, int n, int start){
+        boolean[] visited = new boolean[n+1];
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(start);
+        visited[start] = true;
+        int count =1;
+        
+        while(!queue.isEmpty()){
+            int now = queue.remove();
+            for(int next: graph.get(now)){
+                if (!visited[next]) {
+                    visited[next] = true;
+                    queue.add(next);
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+    
+}
