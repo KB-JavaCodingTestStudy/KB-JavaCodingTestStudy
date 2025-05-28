9bbf47f373e72b8183b1425dc35f14022db496e4
inredzero <96677474+inredzero@users.noreply.github.com>
2025-05-28 22:40:33 +0900

    [PGS] LV_2_전화번호목록_안홍영

--- src/week07/PGS_LV2_전화번호목록/Hong.java
+++ src/week07/PGS_LV2_전화번호목록/Hong.java
@@ -1,1 +1,23 @@
 
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Arrays.sort(phone_book);
+
+        for (int i = 0; i < phone_book.length - 1; i++) {
+            String cur = phone_book[i];
+            String next = phone_book[i + 1];
+
+
+            if (next.length() >= cur.length()) {
+                String s = next.substring(0, cur.length());
+                if (s.equals(cur)) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+}
