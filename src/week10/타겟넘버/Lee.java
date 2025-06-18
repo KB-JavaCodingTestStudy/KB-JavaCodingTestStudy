cd6f63b421847b2a40d659dce834e37417b15b89
Sang-Myeong Lee <teamjuji5@naver.com>
2025-06-18 16:35:18 +0900

    [PGS] 타겟넘버_이상명

--- src/week10/타겟넘버/Lee.java
+++ src/week10/타겟넘버/Lee.java
@@ -1,1 +1,55 @@
 
+/* ================================================================
+ *
+ * Author   : 이상명
+ * Date     : 2025년 00월 00일
+ *
+ * ================================================================
+ * 📌 문제 분석 요약
+ *  -  
+ *  - 
+ *  - 
+ *  
+ * # 입력
+ *  - 
+ *
+ * # 반환
+ *  - 
+ *
+ * 💻 알고리즘 설계
+ *  - 
+ *  - 
+ *  - 
+ *  - 
+ *  - 
+ *
+ * ================================================================
+ */
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+    public int solution(int[] numbers, int target) {
+        
+        int count = dfs(numbers, 0, target, 0);
+        
+        return count;
+    }
+    
+    public int dfs(int[] numbers, int depth, int target, int sum) {
+        if(depth == numbers.length) {
+            if (sum == target)
+                return 1;
+            
+            return 0;
+        }
+        
+        int count = 0;
+        
+        count += dfs(numbers, depth + 1, target, sum + numbers[depth]);
+        count += dfs(numbers, depth + 1, target, sum - numbers[depth]);
+        
+        return count;
+    }
+}
