fad6ca5a90e750d095fa3d497bc8d5b557b9fe53
권채린 <92051742+chaelink@users.noreply.github.com>
2025-06-19 14:50:46 +0900

    [LTC]_twosum_권채린

--- src/week10/twosum/Kwon.java
+++ src/week10/twosum/Kwon.java
@@ -1,3 +1,12 @@
+// 문제
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+// Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+// Example 2:
+// nums 배열 중 두 수를 더했을 때 target이 되는 인덱스 구하기, 정답은 1개만 존재
+
+//반복문, 재귀로 풀 수 있지만 이 문제를 재귀로 풀기에는 투머치라 반복문으로 풀었습니다.
+
 class Solution {
     public int[] twoSum(int[] nums, int target) {
         int[] answer = new int[2];
