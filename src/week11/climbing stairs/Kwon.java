b96ae2f95264411686810db5d30ee258270982dd
권채린 <92051742+chaelink@users.noreply.github.com>
2025-06-23 21:53:02 +0900

    [LTC]_climbingstairs_권채린

--- src/week11/climbing stairs/Kwon.java
+++ src/week11/climbing stairs/Kwon.java
@@ -1,1 +1,24 @@
+import java.util.*;
 
+class Solution {
+    int[] dp;
+
+    public int climbStairs(int n) {
+        dp = new int[n+1];
+
+        dp[1] = 1;
+        if(n>=2) dp[2] = 2;
+
+        if(n>=3) {
+            for(int i=3; i<=n; i++) {
+                dp[i] = dp2(i-1) + dp2(i-2);
+            }
+        }
+
+        return dp[n];
+    }
+
+    public int dp2(int n) {
+        return dp[n];
+    }
+}
