7a6beeef255ac4d5202417648761c5ee3561a6b0
khyej <wl159753@naver.com>
2025-06-25 13:52:19 +0900

    [LTC] 1143_LongestCommonSubsequence_김혜지

---
+++ src/week11/LongestCommonSubsequence/Hye.java
@@ -0,0 +1,36 @@
+package week11.LongestCommonSubsequence;
+
+/* ================================================================
+ *
+ * Problem  : LongestCommonSubsequence_
+ * Date     : 2025년 06월 25일
+ * 
+ * ================================================================
+ */
+
+public class Hye {
+    class Solution {
+        public int longestCommonSubsequence(String text1, String text2) {
+            int n = text1.length();
+            int m = text2.length();
+
+            int[][] dp = new int[n+1][m+1];
+
+            for(int i = 1; i <= n; i++){
+                char ch1 = text1.charAt(i - 1);
+
+                for(int j = 1; j <= m; j++){
+                    char ch2 = text2.charAt(j-1);
+
+                    if(ch1 == ch2){
+                        dp[i][j] = dp[i-1][j-1] + 1;
+                    }else{
+                        dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                    }
+                }
+
+            }
+            return dp[n][m];
+        }
+    }
+}
