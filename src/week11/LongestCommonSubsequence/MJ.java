9fd6c0f26bf0cb15652a4f31ad54fb50dcbe8a72
MJ <nampink2300@gmail.com>
2025-06-25 14:12:11 +0900

    [LTC] 300_LongestIncreasingSubsequence_남민주

---
+++ src/week11/LongestCommonSubsequence/MJ.java
@@ -0,0 +1,26 @@
+package week11.LongestCommonSubsequence;
+
+public class MJ {
+    class Solution {
+        public int lengthOfLIS(int[] nums) {
+            int[] DP= new int[nums.length];
+            int Max=1;
+
+            DP[0]=1;
+            for(int i=1;i<nums.length;i++){
+                for(int j=i-1;j>=0;j--){
+                    if(nums[j]<nums[i]){
+                        DP[i]=Math.max(DP[i], DP[j]);
+                    }
+                }
+                DP[i]++;
+                Max=Math.max(Max, DP[i]);
+            }
+
+            // for(int d:DP){
+            //     System.out.print(d+" ");
+            // }
+            return Max;
+        }
+    }
+}
