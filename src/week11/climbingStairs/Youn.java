afb80deec16c0db1de60a09d615230fb8893ba51
0woy <87132052+0woy@users.noreply.github.com>
2025-06-26 13:33:53 +0900

    Create Youn.java

---
+++ src/week11/climbingStairs/Youn.java
@@ -0,0 +1,20 @@
+class Solution {
+    static Map<Integer,Integer> map;
+    public int climbStairs(int n) {
+        map = new HashMap<>();
+        return dfs(0,n);
+    }
+
+    public static int dfs(int cur, int goal){
+        if(map.containsKey(cur)){
+            return map.get(cur);
+        }
+
+        if(cur >  goal){return 0;}
+        if(cur == goal){return 1;}
+
+        int res =  dfs(cur+1, goal)+dfs(cur+2, goal);
+        map.put(cur, res);        
+        return res;
+    }
+}
