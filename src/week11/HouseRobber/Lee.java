dc51093f3a79bd24df89f0131cc247bd03a00516
Sang-Myeong Lee <teamjuji5@naver.com>
2025-06-26 09:14:40 +0900

    [LTC] HouseRobber_이상명

--- src/week11/HouseRobber/Lee.java
+++ src/week11/HouseRobber/Lee.java
@@ -2,15 +2,16 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        dp[0] = 1;
-        dp[1] = 1;
+    public int rob(int[] nums) {
+        int dp[] = new int[nums.length + 1];
+        dp[0] = 0;
+        dp[1] = nums[0];
 
-        for(int i = 2; i <= n; i++) {
-            dp[i] = dp[i-1] + dp[i-2];
+        for(int i = 1; i < nums.length; i++) {
+
+            dp[i+1] = Math.max(dp[i-1] + nums[i], dp[i]);
         }
 
-        return dp[n];
+        return dp[nums.length];
     }
 }
