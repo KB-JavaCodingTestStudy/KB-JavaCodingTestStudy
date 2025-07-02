267739f1880b215fc4e2e87c5cae00ff5c88691e
권채린 <92051742+chaelink@users.noreply.github.com>
2025-07-02 19:12:10 +0900

    [LTC]_houserobber_권채린

--- src/week12/HouseRobber/Kwon.java
+++ src/week12/HouseRobber/Kwon.java
@@ -2,6 +2,7 @@ class Solution {
     public int rob(int[] nums) {
         //도둑의 집 털기
         //두 집을 연속해서 털면 경찰 출동
+        //주의할 점 : index out of bound 인덱스 범위 주의
 
         int n = nums.length;
         int[] dp = new int[n];
